e0e8dc20e73db1b3ea4e670c0e866ab69d015132
tapish2000 <44476667+tapish2000@users.noreply.github.com>
2021-03-01 20:21:00 +0530

    [Added] Installation part for the readme generator

--- output_readme.md
+++ output_readme.md
@@ -1,2 +1,17 @@
 # readme-toolkit
-The readme-toolkit is a Python repository for blah blah
+The readme-toolkit is a python repository for doing blah.
+
+## Description
+## Installation
+1. Clone the repo
+	```
+	git clone https://github.com/tapish2000/readme-toolkit
+	```
+2. Pull everything
+	```
+	git pull origin main
+	```
+3. Enjoy!
+## Usage
+## Authors and Acknowledgment
+## Contributing
--- readme_gen_input.py
+++ readme_gen_input.py
@@ -6,30 +6,53 @@ def get_repo_name(repo_link):
 
 repo_link = input("Enter the Repository Link: ")
 repo_name = get_repo_name(repo_link)
-maj_lang = input(" What is the major language used in the repository: ")
-purp_repo = input(" What is the purpose of the repository [eg: for timepass]: ")
+maj_lang = input("What is the major language used in the repository: ")
+purp_repo = input("What is the purpose of the repository [eg: for timepass]: ")
+
+install_steps = []
+is_install_req = input("Do you want to add installation steps in the README [y/n]: ").lower()
+write_install = False
+if (is_install_req == 'y' or is_install_req == 'yes'):
+  write_install = True
+i = 1
+while(is_install_req == 'y' or is_install_req == 'yes'):
+  step_desc = input("Write a description for the step [Optional]: ")
+  step_command = input("Write a command associated with the step [Optional]: ")
+
+  if (step_command == ""):
+    install_steps.append(str(i) + ". " + step_desc + "\n")
+  elif (step_desc == ""):
+    install_steps.append(str(i) + ". ```\n" + step_command + "\n```\n")
+  else:
+    install_steps.append(str(i) + ". " + step_desc + "\n\t```\n\t" + step_command + "\n\t```\n")
+  i += 1
+
+  is_install_req = input("Do you want to add more steps to the Installation in the README[y/n]: ").lower()
+
 
 readme_f = open("output_readme.md", "w")
 lines = []
 
 # Repository name and tagline
-lines.append("# " + repo_name)
-lines.append("The " + repo_name + " is a " + maj_lang + " repository " + purp_repo)
+lines.append("# " + repo_name + "\n")
+lines.append("The " + repo_name + " is a " + maj_lang + " repository " + purp_repo + ".\n\n")
 
 # Repository description
-lines.append("# Description")
+lines.append("## Description\n")
 
 # Installation
-lines.append("# Installation")
+if (write_install):
+  lines.append("## Installation\n")
+  lines.extend(install_steps)
 
 # Usage
-lines.append("# Usage")
+lines.append("## Usage\n")
 
 # Authors and Acknowledgment
-lines.append("# Authors and Acknowledgment")
+lines.append("## Authors and Acknowledgment\n")
 
 # Contributing
-lines.append("# Contributing")
+lines.append("## Contributing\n")
 
 # Basic syntax of Markdown
 
