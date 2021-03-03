d083bb7795e9f33e85a3ad553cb604b092763288
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2021-03-03 18:54:03 +0530

    Bug fix and Added wordpool file

--- readme_gen_input.py
+++ readme_gen_input.py
@@ -26,7 +26,6 @@ while(is_install_req == 'y' or is_install_req == 'yes'):
   else:
     install_steps.append(str(i) + ". " + step_desc + "\n\t```\n\t" + step_command + "\n\t```\n")
   i += 1
-
   is_install_req = input("Do you want to add more steps to the Installation in the README[y/n]: ").lower()
 
 
@@ -43,9 +42,9 @@ is_descr_required = input("Do you wish to describe about your repository [y/n]: 
 if(is_descr_required=='y' or is_descr_required=='yes'):
   issue_solved = input("What major issue does your project solve: ")
   use_cases = input("What are the use cases of this project: ")
-  unique_thing1 = input("2 unique ideas in this project which is worth mentioning: \n1. ")
+  unique_thing1 = input("Two unique ideas in this project which is worth mentioning: \n1. ")
   unique_thing2 = input("2. ")
-  lines.append(issue_solved + "\n\n" + use_cases + "\n\n The highlights of the repositories are:\n\n1. " + unique_thing1 +"\n2. " + unique_thing2 + "\n")
+  lines.append(issue_solved + "\n\n The project has been prepared keeping in mind these use cases. " + use_cases + "\n\n The highlights of the repositories are:\n\n1. " + unique_thing1 +"\n2. " + unique_thing2 + "\n")
 
 # Installation
 if (write_install):
@@ -61,15 +60,14 @@ auth = input("Do you wish to tell more about the author of the project [y/n]: ")
 if(auth=='y' or auth=='yes'):
   name = input("Name of the author of this repository: ")
   website_link = input("Link to any personal dev blog/website [optional]: ")
-  ack = input("Acknowledge people who helped you in your project")
+  ack = input("Acknowledge people who helped you in your project: ")
   lines.append("The author of this repository is " + name + ". Explore more about the author and find related things [here](" + website_link + "). This project has also been succesful due to " + ack + "\n\n")
 
 # Contributing
 lines.append("## Contributing\n")
 lines.append("First fork this repo then clone it into your system. Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.\n\n")
 # Basic syntax of Markdown
-lines.appned("\n\n")
-lines.append("<!--- # for title--->\n")
+lines.append("\n\n<!--- # for title--->\n")
 lines.append("<!--- ## for h1--->\n")
 lines.append("<!--- 1. for numberred bullet--->\n")
 lines.append("<!--- - for bullet--->\n")
