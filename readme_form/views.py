37f66fdd37bd46013280fd6bd93e5298c17cf03b
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2021-04-27 22:40:21 +0530

    polishing changes made

--- readme_form/views.py
+++ readme_form/views.py
@@ -118,11 +118,6 @@ def output(request):
     return render(request, 'readme_form/output.html', context)
 
 def download(request):
-    # filename = 'output_README.md'
-    # readme_f = open(filename, 'r')
-    # response = FileResponse(readme_f.read(), content_type='file/markdown')
-    # response['Content-Length'] = os.path.getsize(filename)
-    # response['Content-Disposition'] = "attachment; filename=%s" % filename
-    views_dir = os.path.dirname(__file__)
-    filepath = views_dir + '/readme/output_README.md'
+    dir_path = os.path.dirname(__file__)
+    filepath = dir_path + '/readme/output_README.md'
     return serve(request, os.path.basename(filepath),os.path.dirname(filepath))
